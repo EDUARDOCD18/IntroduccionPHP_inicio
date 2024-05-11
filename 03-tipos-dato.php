a77e9944b56a11625a34e2c29f879c266005210f
eduardocd18 <javierecd@gmail.com>
2024-05-11 11:54:03 -0400

    255. Tipos de Dato en PHP

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,23 @@
 <?php include 'includes/header.php';
 
+/* -- Boolean -- */
+$logeado = false;
+var_dump($logeado);
 
+/* -- Enteros -- */
+$numero = 45;
+var_dump($numero); 
 
+/* -- Flotantes -- */
+$float= 3.13;
+var_dump($float);
+
+/* -- Strings --  */
+$nombre = "Javi";
+var_dump($nombre);
+
+/* -- Arreglos-- */
+$array = [];
+var_dump($array);
 
 include 'includes/footer.php';
