413592846093ce22ec0c68dddf8a10d633e6f8d5
eduardocd18 <javierecd@gmail.com>
2024-05-11 11:11:31 -0400

    254. Variables y Constantes en PHP

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,21 @@
 <?php include 'includes/header.php';
 
+$nombre = "Javier ";
+echo $nombre;
+var_dump($nombre);
 
+$nombre = "Wili ";
+echo $nombre;
 
+define("constante", "Valor de constante ");
+echo constante;
+
+const constante2 = "Hola2 ";
+echo constante2;
+
+$nombreCliente = "Katara ";
+$nombre_cliente = "Vader ";
+echo $nombre_cliente;
+echo $nombreCliente;
 
 include 'includes/footer.php';
