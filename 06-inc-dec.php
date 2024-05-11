799b92f2f6c00cd6cff6d64b507db5421d85b50b
eduardocd18 <javierecd@gmail.com>
2024-05-11 12:37:22 -0400

    258. Incrementos y decrementos en PHP

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
+$numero1 = 30;
+echo $numero1;
+echo "<br>";
 
+/* Incremento */
+$numero1++;
+echo $numero1;
+echo "<br>";
+
+/* Incremento con saltos de valores variables */
+$numero1 += 5;
+echo $numero1;
+echo "<br>";
+
+/* Decremento */
+$numero1--;
+echo $numero1;
+echo "<br>";
+
+/* Decremento con saltos de valores variables */
+$numero1 -= 20;
+echo $numero1;
+echo "<br>";
 
 
 include 'includes/footer.php';
