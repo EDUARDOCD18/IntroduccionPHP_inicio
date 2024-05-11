9af7586f1c0ed31f096614d39303825bf229a295
eduardocd18 <javierecd@gmail.com>
2024-05-11 13:13:01 -0400

    258. Incrementos en PHP

--- 07-string-methods.php
+++ 07-string-methods.php
@@ -1,6 +1,46 @@
 <?php include 'includes/header.php';
 
+$nombreCliente = " Javier Eduardo ";
 
+/* Conocer la extensión de un string */
+echo "Extensión de ", strlen($nombreCliente), " caracteres.";
+echo "<br>";
+var_dump($nombreCliente);
+echo "<br>";
 
+/* Eliminar espacios en blanco al principio y al final */
+$texto = trim($nombreCliente);
+echo strlen($texto);
+echo "<br>";
+
+/* Texto a mayúsculas */
+echo strtoupper($texto);
+echo "<br>";
+
+/* Texto a minúscula */
+echo strtolower($texto);
+echo "<br>";
+
+$mail1 = "correo@correo.com";
+$mail2 = "Correo@correo.com";
+var_dump($mail1 === $mail2);
+echo "<br>";
+var_dump(strtolower($mail1) === strtolower($mail2));
+echo "<br>";
+
+/* Reemplazar valores en cadenas de texto */
+echo str_replace("Javier", "Javi", $nombreCliente);
+echo "<br>";
+
+/* Revisar sin un string existe o no */
+echo strpos($nombreCliente, "Javi");
+echo "<br>";
+
+/* Templates string */
+$tipoCliente = "Premium";
+echo "El cliente " .  $nombreCliente .  " es " . $tipoCliente;
+echo "<br>";
+
+echo "El cliente {$nombreCliente} es del tipo {$tipoCliente}";
 
 include 'includes/footer.php';
