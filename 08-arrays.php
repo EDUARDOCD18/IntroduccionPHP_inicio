0175aecc1af817b5f005543728c297cc2f458fb6
eduardocd18 <javierecd@gmail.com>
2024-05-12 19:19:14 -0400

    260. Arreglos y Funciones para Arreglos en PHP

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,31 @@
 <?php include 'includes/header.php';
 
+$carrito = ['tablet', 'tv', 'pc'];
+var_dump($carrito);
 
+/* Para ver los contenidos de un array */
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
 
+/* Acceder a los elementos de un array */
+echo "$carrito[2]";
+echo "<br>";
+
+/* Agregar al array de manera manual por el número de índice */
+$carrito[3] = "Phone";
+echo ($carrito[3]);
+
+/* Agregar elementos al final del array */
+array_push($carrito, "Head");
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
+
+/* Añadir elementos el inicio del arreglo */
+array_unshift($carrito, "Smart watch");
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
 
 include 'includes/footer.php';
