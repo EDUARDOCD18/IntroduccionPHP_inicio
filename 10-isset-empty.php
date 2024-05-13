54f7630b8b193d2da8089c833e230d6846fe793c
eduardocd18 <javierecd@gmail.com>
2024-05-12 20:10:57 -0400

    262. isset() y empty()

--- 10-isset-empty.php
+++ 10-isset-empty.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
+$clientes = [];
+$clientes2 = array();
+$clientes3 = array("Pedro", "javier", "Katara");
+$cliente = [
+    "nombre" => "javier",
+    "saldo" => 200
+];
 
+/* Función empty - Revisa si el array está vació */
+var_dump(empty($clientes));
+echo "<br>";
+var_dump(empty($clientes2));
+echo "<br>";
+var_dump(empty($clientes3));
+echo "<br>";
 
+/* Isset - Revisa si un arreglo ha sido creado o una propiedad ha sido definada */
+var_dump(isset($clientes));
+echo "<br>";
+var_dump(isset($clientes4));
+echo "<br>";
+var_dump(isset($cliente["nombre"]));
+echo "<br>";
+var_dump(isset($cliente["cod"]));
 
 include 'includes/footer.php';
