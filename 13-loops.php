c61c6c8234b6983f186284d83452e81673ad21ab
eduardocd18 <javierecd@gmail.com>
2024-05-14 18:51:15 -0400

    266. while, do while y foreach

--- 13-loops.php
+++ 13-loops.php
@@ -3,10 +3,11 @@
 /* While loop */
 /* $i = 0;
 
-while ($i < 10) {
+while ($i < 10) :
     echo $i . "<br>";
     $i++;
-}
+endwhile;
+
 echo "<br>"; */
 
 /* do while */
@@ -24,15 +25,34 @@ echo "<br>"; */
 echo "<br>"; */
 
 /* Algoritmo fizz buzz */
-for ($i = 1; $i <= 60; $i++) {
-    if ($i % 3 === 0 && $i % 5 === 0) {
+/* for ($i = 1; $i <= 60; $i++) :
+    if ($i % 3 === 0 && $i % 5 === 0) :
         echo $i . "- FizzBuzz" . "<br>";
-    } elseif ($i % 3 === 0) {
+    elseif ($i % 3 === 0) :
         echo $i . "- Fizz" . "<br>";
-    } elseif ($i % 5 === 0) {
+    elseif ($i % 5 === 0) :
         echo $i . "- Buzz" . "<br>";
-    } else {
+    else :
         echo $i . "<br>";
-    }
-}
+    endif;
+
+endfor; */
+
+
+/* For each loop */
+$clientes = ['javi', 'Wili', 'kata'];
+
+foreach ($clientes as $cliente) :
+    echo '- ' . $cliente . '<br>';
+endforeach;
+
+$cliente = array(
+    'nombre' => "javier",
+    'saldo' => 500,
+    'tipo' => "pre"
+);
+
+foreach ($cliente as $key => $valor) :
+    echo $key . ": " . $valor . "<br>";
+endforeach;
 include 'includes/footer.php';
