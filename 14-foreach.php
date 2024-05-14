2a9f66ecc3d2844986cb2c8a5008ec7a6b6dabfe
eduardocd18 <javierecd@gmail.com>
2024-05-14 19:30:13 -0400

    267. foreach y mostrar el contenido de un array

--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,32 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'tablet',
+        'precio' => '200',
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'tv',
+        'precio' => '2000',
+        'disponible' => false
+    ],
+    [
+        'nombre' => 'ipod',
+        'precio' => '1500',
+        'disponible' => true
+    ]
+];
 
+foreach ($productos as $producto) { ?>
+    <li>
+        <p>Producto: <?php echo $producto["nombre"]; ?></p>
+        <p>Precio: <?php echo "$" . $producto["precio"]; ?> </p>
+        <p><?php echo ($producto["disponible"]) ? "Disponible" : "No Disponible"; ?></p>
+    </li>
+<?php
+
+}
 
 
 include 'includes/footer.php';
