71db3170a10060a60ac99ba7e769072aab03be13
eduardocd18 <javierecd@gmail.com>
2024-05-13 20:41:47 -0400

    265. for loops

--- 13-loops.php
+++ 13-loops.php
@@ -1,6 +1,38 @@
 <?php include 'includes/header.php';
 
+/* While loop */
+/* $i = 0;
 
+while ($i < 10) {
+    echo $i . "<br>";
+    $i++;
+}
+echo "<br>"; */
 
+/* do while */
+/* $i = 0;
+do {
+    echo $i . "<br>";
+    $i++;
+} while ($i <= 10);
+echo "<br>"; */
 
+/* for loop */
+/* for ($i = 0; $i < 10; $i++) {
+    echo $i . "<br>";
+}
+echo "<br>"; */
+
+/* Algoritmo fizz buzz */
+for ($i = 1; $i <= 60; $i++) {
+    if ($i % 3 === 0 && $i % 5 === 0) {
+        echo $i . "- FizzBuzz" . "<br>";
+    } elseif ($i % 3 === 0) {
+        echo $i . "- Fizz" . "<br>";
+    } elseif ($i % 5 === 0) {
+        echo $i . "- Buzz" . "<br>";
+    } else {
+        echo $i . "<br>";
+    }
+}
 include 'includes/footer.php';
